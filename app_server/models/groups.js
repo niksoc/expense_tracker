ef0bd41f44e9f454c4109f8d1e057e1d1881b756
niksoc <nikhil_2811@hotmail.com>
2016-03-26 19:34:23 +0530

    adding authentication

--- app_server/models/groups.js
+++ app_server/models/groups.js
@@ -1,9 +1,30 @@
 var mongoose=require('mongoose');
-
+var crypto=require('crypto');
+var jwt=require('jsonwebtoken');
 var usersSchema=new mongoose.Schema({
     username:String,
-    balance:Number
+    balance:Number,
+    hash:String,
+    Salt:String
 });
+usersSchema.methods.setPassword=function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+usersSchema.methods.validPassword=function(password){
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    return hash===this.hash;
+};
+usersSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+    return jwt.sign({
+	_id: this._id,
+	email: this.email,
+	name: this.name,
+	exp: parseInt(expiry.getTime() / 1000) 
+    }, process.env.JWT_SECRET);
+};
 var transactionsSchema=new mongoose.Schema({
     amount:{type:Number, min:0},
     date:{type:Date, "default":Date.now},
